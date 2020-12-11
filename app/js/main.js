32c00b98e4f384019263f693627f9f8a46309da8
evgeny3322 <retard_x_98@mail.ru>
2020-12-11 16:58:58 +0300

    adaptive+shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,42 @@
 $(function(){
+    
+$('.shop-content__filter-btn').on('click', function() { 
+        $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+        $(this).addClass('shop-content__filter-btn--active');
+    });
+    
+$('.button-list').on('click', function (){
+        $('.product-item').addClass('product-item--list')
+    });
+    
+$('.button-grid').on('click', function (){
+        $('.product-item').removeClass('product-item--list')
+    });
+
+$('.header__circle').on('click', function(){
+     $('.menu-bar').toggleClass('menu-bar--active');
+});
+
+$('.menu-bar__img').on('click', function(){
+    $('.menu-bar').toggleClass('menu-bar--active');
+});
+   
+    
+$('.select-style').styler();
+    
+    
 $('.filter-price__input').ionRangeSlider({
-    type: "double",
-    prefix: "$",
-    step: 0.01,
-onStart: function (data) {
-    $( '.filter-price__from').text(data.from);
-    $( '.filter-price__to').text(data.to);
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            $( '.filter-price__from').text(data.from);
+            $( '.filter-price__to').text(data.to);
         },
-    onChange: function (data) {
-        $( '.filter-price__from').text(data.from);
-        $( '.filter-price__to').text(data.to);
-    },
-})
+        onChange: function (data) {
+            $( '.filter-price__from').text(data.from);
+            $( '.filter-price__to').text(data.to);
+        },
+    })
 
 
 $(".star").rateYo({
@@ -20,13 +45,20 @@ $(".star").rateYo({
     ratedFill: "#ffc35b",
   });
 
+$(".product__items-star").rateYo({
+    starWidth: "16px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+});
+
 
 $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000 
- });
+        // autoplay: true,
+        // autoplaySpeed: 2000 
+});
+
 
 });
 
@@ -41,8 +73,3 @@ var mixer = mixitup('.product', {
         control: '.products__btn'
     }
 });
-
-
-
-
- 
